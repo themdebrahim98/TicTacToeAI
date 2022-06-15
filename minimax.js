4ba6398c7ef5d29b5d5ac37742db39e649bf5746
mdebrahim98 <mdebrahim742148@gmail.com>
2022-06-15 11:33:58 +0530

    some stle changes

--- minimax.js
+++ minimax.js
@@ -1,144 +1,134 @@
-// let boxes = document.getElementsByClassName("box");
-// console.log(boxes)
-
 let board = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
 ];
-
-// boxes[0].childNodes[0].innerHTML = 'x'
 
 let ai = "X";
 let human = "O";
 let currentPlayer = human;
 
 function equals3(a, b, c) {
-  return a == b && b == c && a != "";
+    return a == b && b == c && a != "";
 }
 
 function checkWinnerInRecurson() {
-  let winner = null;
+    let winner = null;
 
-  // horizontal
-  for (let i = 0; i < 3; i++) {
-    if (equals3(board[i][0], board[i][1], board[i][2])) {
-      winner = board[i][0];
+    // horizontal
+    for (let i = 0; i < 3; i++) {
+        if (equals3(board[i][0], board[i][1], board[i][2])) {
+            winner = board[i][0];
+        }
     }
-  }
 
-  // Vertical
-  for (let i = 0; i < 3; i++) {
-    if (equals3(board[0][i], board[1][i], board[2][i])) {
-      winner = board[0][i];
+    // Vertical
+    for (let i = 0; i < 3; i++) {
+        if (equals3(board[0][i], board[1][i], board[2][i])) {
+            winner = board[0][i];
+        }
     }
-  }
 
-  // Diagonal
-  if (equals3(board[0][0], board[1][1], board[2][2])) {
-    winner = board[0][0];
-  }
-  if (equals3(board[2][0], board[1][1], board[0][2])) {
-    winner = board[2][0];
-  }
-
-  let openSpots = 0;
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (board[i][j] == "") {
-        openSpots++;
-      }
+    // Diagonal
+    if (equals3(board[0][0], board[1][1], board[2][2])) {
+        winner = board[0][0];
     }
-  }
+    if (equals3(board[2][0], board[1][1], board[0][2])) {
+        winner = board[2][0];
+    }
 
-  if (winner == null && openSpots === 0) {
-    return "tie";
-  } else {
-    return winner;
-  }
+    let openSpots = 0;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] == "") {
+                openSpots++;
+            }
+        }
+    }
+
+    if (winner == null && openSpots === 0) {
+        return "tie";
+    } else {
+        return winner;
+    }
 }
 
-function findBestMove() {
-  // AI to make its turn
-   let box = document.getElementsByClassName("box");
+function findBestMove(board) {
+    // heare AI is Maximizer player
+    // AI will maximize the score (respect to AI )
+    let box = document.getElementsByClassName("box");
 
+    let bestScore = -Infinity;
+    let move;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            // Is the spot available?
+            if (board[i][j] == "") {
+                board[i][j] = ai;
+                let score = minimax(board, 0, false); //oponent  will minimize the  score (humon)
+                board[i][j] = "";
+                if (score > bestScore) {
+                    bestScore = score;
 
-  let bestScore = -Infinity;
-  let move;
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      // Is the spot available?
-      if (board[i][j] == "") {
-        board[i][j] = ai;
-        let score = minimax(board, 0, false); //human will make it turns
-        board[i][j] = "";
-        if (score > bestScore) {
-          bestScore = score;
-          move = { i, j };
+                    move = { i, j };
+                }
+            }
         }
-      }
     }
-  }
-  board[move.i][move.j] = ai;
+    board[move.i][move.j] = ai;
 
-  Array.from(box).forEach((cell) => {
-    //   cell.classList.remove(XcLASS);
-    //   cell.classList.remove(CIRCLECLASS);
-    //   cell.classList.remove("clrchange");
-    if (cell.id == `${move.i}${move.j}`) {
-      // cell.classList.add("x");
-      // cell.innerHTML = 'djkvbhjdfb'
-      cell.childNodes[0].innerHTML = ai;
+    Array.from(box).forEach((cell) => {
+        if (cell.id == `${move.i}${move.j}`) {
+            cell.childNodes[0].innerHTML = ai;
+        }
+    });
 
-      // text_to_change.nodeValue = 'new text';
-    }
-  });
-
-  console.log(board);
-  currentPlayer = human;
+    currentPlayer = human;
 }
 
 let scores = {
-  X: 10,
-  O: -10,
-  tie: 0,
+    X: 10,
+    O: -10,
+    tie: 0,
 };
 
 function minimax(board, depth, isMaximizing) {
-  let result = checkWinnerInRecurson();
-  if (result !== null) {
-    return scores[result];
-  }
+    let result = checkWinnerInRecurson();
+    if (result !== null) {
+        return scores[result];
+    }
 
-  if (isMaximizing) {
-    let bestScore = -Infinity;
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        // Is the spot available?
-        if (board[i][j] == "") {
-          board[i][j] = ai;
-          let score = minimax(board, depth + 1, false); // ai will make its turn
-          board[i][j] = "";
-          bestScore = Math.max(score, bestScore);
+    if (isMaximizing) {
+        // AI maximize the score
+        let bestScore = -Infinity;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                // Is the spot available?
+                if (board[i][j] == "") {
+                    board[i][j] = ai;
+                    let score = minimax(board, depth + 1, false); // Human will minimize
+                    board[i][j] = "";
+                    bestScore = Math.max(score, bestScore);
+                }
+            }
         }
-      }
-    }
-    return bestScore;
-  } else {
-    let bestScore = Infinity;
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        // Is the spot available?
-        if (board[i][j] == "") {
-          board[i][j] = human;
-          let score = minimax(board, depth + 1, true);
-          board[i][j] = "";
-          bestScore = Math.min(score, bestScore);
+        return bestScore;
+    } else {
+        // human(oponent) minimize the score( respect to AI)
+        let bestScore = Infinity;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                // Is the spot available?
+                if (board[i][j] == "") {
+                    board[i][j] = human;
+                    let score = minimax(board, depth + 1, true);
+                    board[i][j] = "";
+                    bestScore = Math.min(score, bestScore);
+                }
+            }
         }
-      }
+        return bestScore;
     }
-    return bestScore;
-  }
 }
 
 // boxes.forEach((cell) => {
@@ -147,27 +137,25 @@ function minimax(board, depth, isMaximizing) {
 //   //   cell.classList.remove("clrchange");
 // });
 
-// findBestMove();
-findBestMove()
 function mousePressed(e) {
-  // const winingMessage = document.getElementById("winingMessage");
-  let cellidArray = e.target.id.split("");
-  board[cellidArray[0]][cellidArray[1]] = human;
-  console.log(board);
-  // e.target.classList.add("circle");
-  let bestmove = findBestMove();
+    // const winingMessage = document.getElementById("winingMessage");
+    let cellidArray = e.target.id.split("");
 
-  let result = checkWinnerInRecurson();
-  if (result != null) {
-    if (result == "tie") {
-      console.log("tie");
-      console.log( document.getElementById("win").innerHTML = 'tie');
-    } else {
-      console.log(`wins ${result}`);
+    board[cellidArray[0]][cellidArray[1]] = human;
+    document.getElementById(e.target.id).style.color = "white";
 
-      
+    console.log(board);
+    // e.target.classList.add("circle");
+
+    let result = checkWinnerInRecurson();
+    if (result != null) {
+        if (result == "tie") {
+            console.log("tie");
+            document.getElementById("win").innerHTML = "tie";
+            document.getElementById("reset").style.display = "block";
+        } else {
+            console.log(`wins ${result}`);
+        }
     }
-  }
-
- 
+    findBestMove(board);
 }
--- script.js
+++ script.js
@@ -1,51 +1,66 @@
 let boxes = document.getElementsByClassName("box");
+let countx = document.getElementsByClassName('countx')[0];
+let counto = document.getElementsByClassName('counto')[0];
+let overshadow = document.getElementsByClassName('overshadow')[0];
+let restart = document.getElementById('restart');
+
+
 
 console.log("Welcome to Tic Tac Toe");
 let music = new Audio("music.mp3");
 let audioTurn = new Audio("ting.mp3");
-let gameover = new Audio("gameover.mp3");
-let turn = "0";
+let gameover = new Audio("wining.mp3");
+let turn = "O";
 let isgameover = false;
+let xwin = 0;
+let owin = 0;
 
-// Function to change the turn
-const changeTurn = () => {
-  //   return turn === "X" ? "0" : "X";
-  //   findBestMove();
-};
+
 
 // Function to check for a win
 const checkWin = () => {
   let boxtext = document.getElementsByClassName("boxtext");
-    var wins = [
-      [0, 1, 2, 5, 5, 0],
-      [3, 4, 5, 5, 15, 0],
-      [6, 7, 8, 5, 25, 0],
-      [0, 3, 6, -5, 15, 90],
-      [1, 4, 7, 5, 15, 90],
-      [2, 5, 8, 15, 15, 90],
-      [0, 4, 8, 5, 15, 45],
-      [2, 4, 6, 5, 15, 135],
-    ];
- 
+  var wins = [
+    [3, 4, 5, 0, 128, 0],
+    [6, 7, 8, 0, 216, 0],
+    [0, 1, 2, 0, 34, 0],
+    [0, 3, 6, -90, 127, 90],
+    [1, 4, 7, 0, 127, 90],
+    [2, 5, 8, 90, 127, 90],
+    [0, 4, 8, 0, 125, 45],
+    [2, 4, 6, 0, 131, 135],
+  ];
+
   wins.forEach((e) => {
     if (
       boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
       boxtext[e[2]].innerText === boxtext[e[1]].innerText &&
       boxtext[e[0]].innerText !== ""
     ) {
-      document.querySelector(".info").innerText =
-        boxtext[e[0]].innerText + " Won";
-      document.getElementById("win").innerHTML = "";
 
+
+
+
+
+      document.querySelector(".container .line").style.width = "100%";
+      document.querySelector(".container .line").style.transform = `translate(${e[3]}px, ${e[4]}px) rotate(${e[5]}deg)`;
+      console.log(e, "element");
+      document.getElementById("imgbox").querySelector('img').style.display = "initial";
+      console.log(document.getElementById("imgbox").querySelector('img'), "sdklvncjksb");
       isgameover = true;
-      document
-        .querySelector(".imgbox")
-        .getElementsByTagName("img")[0].style.width = "200px";
-      document.querySelector(
-        ".line"
-      ).style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
-      document.querySelector(".line").style.width = "20vw";
+      document.getElementById('reset').style.display = "block"
+      console.log(boxtext)
+      document.getElementById("win").innerHTML = `Win ${boxtext[e[0]].innerText}`
+      if (boxtext[e[0]].innerText === "X") {
+        console.log(++xwin);
+        countx.querySelector('span').innerText = xwin;
+      } else if (boxtext[e[0]].innerText == "O") {
+        console.log(owin++);
+      }
+      overshadow.style.display = "block"
+      gameover.play();
     }
+
   });
 };
 
@@ -57,30 +72,17 @@ Array.from(boxes).forEach((element) => {
   element.addEventListener("click", (e) => {
     if (boxtext.innerText === "") {
       boxtext.innerText = turn;
-      //   Array.from(boxes).forEach((elm)=>{
-      //       if (boxtext.innerText === "") {
-      //         mousePressed(e);
-      //       }
-      //   })
 
-      //   turn = changeTurn();
       mousePressed(e);
-      audioTurn.play();
+
+
       checkWin();
-      if (!isgameover) {
-        document.getElementsByClassName("info")[0].innerText =
-          "Turn for " + turn;
-      }
+
     }
   });
 });
 
-// draw
-// function draw() {
-//   for (let i = 0; i < board.length; i++) {
-//     for (let j = 0; j < board.length; j++) {}
-//   }
-// }
+
 
 // Add onclick listener to reset button
 
@@ -94,13 +96,41 @@ reset.addEventListener("click", () => {
       board[i][j] = "";
     }
   }
-  findBestMove();
+
   document.getElementById("win").innerHTML = "";
 
-  turn = "0";
+  turn = "O";
   isgameover = false;
-  document.querySelector(".line").style.width = "0vw";
-  document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
-  document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width =
-    "0px";
+  document.querySelector(".line").style.width = "0px";
+  document
+    .getElementById("imgbox")
+    .querySelector("img").style.display = "none";
+  overshadow.style.display = "none"
 });
+
+restart.addEventListener('click', () => {
+  xwin = 0;
+  owin = 0;
+  counto.querySelector('span').innerText = owin;
+  countx.querySelector("span").innerText = xwin;
+  console.log("ajilhfh")
+  let boxtexts = document.querySelectorAll(".boxtext");
+  Array.from(boxtexts).forEach((element) => {
+    element.innerText = "";
+  });
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      board[i][j] = "";
+    }
+  }
+
+  document.getElementById("win").innerHTML = "";
+
+  turn = "O";
+  isgameover = false;
+  document.querySelector(".line").style.width = "0px";
+  document
+    .getElementById("imgbox")
+    .querySelector("img").style.display = "none";
+  overshadow.style.display = "none"
+})
